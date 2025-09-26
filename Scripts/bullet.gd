de47e0474e938246bf1311103019f3b467c08a90
Titus <titus@Krusty.local>
2025-09-26 09:33:49 -0500

    maybe better

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,8 +1,8 @@
 extends Area2D
 var Direction = 0
 var Speed = 100
+var Player_Position = Vector2(0,0)
 
-var Width = get_viewport_rect().size.x
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var Player_position
@@ -11,8 +11,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var Direction = get_node("/root/Main/Player").Facing
-	var Player_position_x = get_node("/root/Main/Player").Position.x
+	var Player_Position = get_node("/root/Main/Player").position
 func spawn():
+	self.position = Player_Position
+	var Width = get_viewport_rect().size.x
 	if position.x > 0 or position.x < Width:
 		position.x += Speed*Direction
 	else:
